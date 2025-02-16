f9b8cdda851f74190ae67ddcffc2e0f545516125
ganeshwadule <uniquegbw09@gmail.com>
2025-02-16 19:22:56 +0530

    basics of js

---
+++ Javascript/index.js
@@ -0,0 +1,20 @@
+function eligible(users){
+    const eligibleUsers = [];
+    for(let user of users){
+        if(user.age >= 18 && user.gender.toLowerCase() == 'male'){
+            eligibleUsers.push(user);
+        }
+    }
+
+    return eligibleUsers;
+}
+
+const users =[
+    {fullName:"Ganesh Wadule",age:22,gender:"male"},
+    {fullName:"Vaishnavi Wadule",age:17,gender:"female"},
+    {fullName:"Gita Wadule",age:42,gender:"female"},
+    {fullName:"Bandu Wadule",age:46,gender:"male"},
+    {fullName:"Punjabai Wadule",age:22,gender:"female"}
+];
+
+console.log(eligible(users));
