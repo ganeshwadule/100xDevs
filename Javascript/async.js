be55c82cf8afb5bc5facdc84f081b926a8d7e3ac
ganeshwadule <uniquegbw09@gmail.com>
2025-02-18 23:30:56 +0530

    promise

--- Javascript/async.js
+++ Javascript/async.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("./data.txt", "utf8");
+fs.readFile("./data.txt", "utf8",(err,data)=>console.log(data));
 
-console.log(data);
+// console.log(data);
+fs.readFile("data.json", "utf8",(err,data)=>console.log(data));
+// console.log(jsonData);
 
-const jsonData = fs.readFileSync("data.json", "utf8");
-console.log(jsonData);
+function calc(a,b,fun){
+    fun(a,b);
+}
+
+const sum = (a,b)=>{console.log(a+b)}
+calc(3,4,sum);
