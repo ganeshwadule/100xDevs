e401b1ed738da635a375894467faedf7f85b19ae
ganeshwadule <uniquegbw09@gmail.com>
2025-02-19 21:53:13 +0530

    created own promise class

--- Javascript/my_promise_class.js
+++ Javascript/my_promise_class.js
@@ -1,1 +1,36 @@
-// assignement
+const fs = require('fs');
+
+class promise {
+    constructor(fn) {
+        // Store the function but don't execute it yet
+        this.fn = fn;
+    }
+
+    then(resolve) {
+        this.resolve = resolve;
+        
+        // Now that resolve is set, execute fn
+        this.fn((data) => {
+            if (this.resolve) {
+                this.resolve(data);
+            }
+        });
+    }
+}
+
+function readFileAsync(printData) {
+    fs.readFile("data.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
+        printData(data);
+    });
+}
+
+function printData(data) {
+    console.log(data);
+}
+
+const p = new promise(readFileAsync);
+p.then(printData); // Ensuring resolve is set before execution
