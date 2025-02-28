54b1a03b748c8fe8a3bc769f9d04d11c96e04675
ganeshwadule <uniquegbw09@gmail.com>
2025-02-28 17:36:04 +0530

    digital clock using recursive setTimeout

--- Javascript/dom/script.js
+++ Javascript/dom/script.js
@@ -1,7 +1,23 @@
  
 const timeEl = document.querySelector("#time");
 let val = 0;
-setInterval(()=>{
-    timeEl.innerHTML = val;
-    val += 1;
-},1000);
+// setInterval(()=>{
+//     timeEl.innerHTML = val;
+//     val += 1;
+// },1000);
+
+function recursiveCounter(){
+   timeEl.innerHTML = new Date().toLocaleTimeString();
+   
+   setTimeout(recursiveCounter,1000)
+}
+
+// setTimeout(recursiveCounter(0),1000)
+// callback 
+// setInterval(()=>console.log("Hello"),1000)
+const callback = ()=>{
+    console.log("Hii I am timout")
+    setTimeout(callback,1000)
+}
+
+setTimeout(recursiveCounter,1000)
