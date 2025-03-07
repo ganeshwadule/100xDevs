2873be1836342201cb643f43fdaa006f8f51beec
ganeshwadule <uniquegbw09@gmail.com>
2025-03-07 23:04:27 +0530

    middlware assignment done

---
+++ express/middleware-assignment.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+
+app.use((req,res,next)=>{
+    console.log(new Date())
+    console.log(req.url)
+    console.log(req.method)
+    next()  // call to the actual route handler
+})
+app.post("/api/data",(req,res)=>{
+    
+    res.send("Hello world!");
+})
+
+app.put("/api/data",(req,res)=>{
+    console.log(req.url)
+    console.log(req.method)
+    res.send("Hello world!");
+})  
+
+app.listen(3070,()=>console.log("Hii"))
