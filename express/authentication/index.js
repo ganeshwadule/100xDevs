864b3bba5192186465631b1118d90c1eb857168f
ganeshwadule <uniquegbw09@gmail.com>
2025-03-11 04:31:00 +0530

    created authenticated endpoint using tokens

---
+++ express/authentication/index.js
@@ -0,0 +1,70 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+function generateToken(){
+    const chars = [...'abcdefghijklmnopqrstuvwxyz',
+        ...'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        ..."0123456789"
+    ]
+    // console.log(chars[26])
+    let token = ""
+
+    for(let i=0;i<32;i++){
+        token += chars[Math.floor(Math.random()*chars.length)];
+    }
+    return token;
+    
+}
+generateToken()
+app.post("/signup",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(u=> u.username === username);
+    if(user)
+        return res.json({message:"user already present"});
+    users.push({
+        username:username,
+        password:password
+    })
+    return res.json({message:"signup user successfully"});
+})
+
+app.post("/signin",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    const user = users.find(u => u.username === username);
+    
+    if(!user)
+        return res.json({message:"user not present"})
+    if(user.password !== password)
+        return res.json({message:'incorrect password'})
+    console.log(user)
+    const token = generateToken();
+    user.token=token;
+    console.log(user)
+    return res.json({message:token})
+
+})
+
+// First authenticated endpoint
+app.get("/me",(req,res)=>{
+    const token = req.headers.token;
+    const user = users.find(u => u.token === token);
+
+    if(!user)
+        return res.status(401).json({message:"Unauthorized"})
+    
+    res.json({userInfo:{
+        username:user.username,
+        password:user.password
+    }})
+})
+
+app.listen(3000,()=>console.log("server is running"))
