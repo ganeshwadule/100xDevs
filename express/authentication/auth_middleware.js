4b80759c40fecd8acadc34e167644e8d39810c5a
ganeshwadule <uniquegbw09@gmail.com>
2025-03-18 17:17:24 +0530

    basic auth service

--- express/authentication/auth_middleware.js
+++ express/authentication/auth_middleware.js
@@ -1,69 +1,56 @@
-const express = require("express")
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const path = require("path")
+const app = express();
 
-const app = express()
-const JWT_SECRET_KEY = "MeBillionaire"
+app.use(express.json());
 
-app.use(express.json())
-const users = []
-
-
-
-
-app.post("/signup",(req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const user = users.find(u => u.name === username);
-    if(user)
-        return res.json({message:"user already present"})
-
-    users.push({
-        username:username,password:password
-    })
-    res.json({message:"you are signed up"})
+const users = [];
+const JWT_SECRET = "I am the Best";
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
 })
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.post("/signin",(req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log(users)
-    const user = users.find(u => u.username === username && u.password === password);
-    console.log(user)
-    if(!user)
-        return res.json({message:"user doesn't exists"})
+  const user = users.find((u) => u.username === username);
 
-    const token = jwt.sign({username},JWT_SECRET_KEY);
+  if (user) return res.json({ message: "User already exists" });
 
-    res.json({token:token})
-})
+  users.push({ username, password });
+  res.json({ message: "You are signed up" });
+});
 
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-//auth middleware
-function auth(req,res,next){
-    if(!req.headers.token){
-        return res.json({message:"You are unathorized"})
-    }
-    const token = req.headers.token;
-    const decodedUser = jwt.verify(token,JWT_SECRET_KEY)
+  const user = users.find((u) => u.username === username);
 
-    // const user = users.find(u=> u.username === decodedUser.username)
-    if(!decodedUser.username)
-        return res.json({message:"You are unathorized"})
-    req.username = decodedUser.username
-    next()
-}
-// 
-// app.use(auth)
+  if (!user) return res.json({ message: "User doesn't exists" });
 
-app.get("/me",auth,(req,res)=>{
-   
-    const user = users.find(u => u.username == req.username);
+  const token = jwt.sign({ username }, JWT_SECRET);
 
-    if(!user)
-        return res.status(401).json({message:"Unauthorized"});
+  res.json({ token });
+});
 
-    return res.json({username:user.username,password:user.password})
-})
+app.use((req, res, next) => {
+  if (!req.headers.token) return res.json({ message: "Unauthorized" });
+  const token = req.headers.token;
+  const decodedUsser = jwt.verify(token, JWT_SECRET);
+  if(!decodedUsser.username)return res.json({message:"Invalid User"})
+  req.username = decodedUsser.username;
+  next();
+});
 
-app.listen(3000,()=>console.log("server is listening"))
+app.get("/me", (req, res) => {
+  const user = users.find((u) => u.username === req.username);
+  if (!user) return res.json({ message: "Unauthorized" });
+  res.json({
+    username: user.username,
+    password: user.password,
+  });
+});
+
+app.listen(3000, () => console.log("server is listening"));
