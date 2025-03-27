992e4549b5f871d1964098e70199413282a6da01
ganeshwadule <uniquegbw09@gmail.com>
2025-03-27 16:19:40 +0530

    used useState,useEffect & conditional rendering

--- react-apps/react-practice/src/App.jsx
+++ react-apps/react-practice/src/App.jsx
@@ -1,28 +1,40 @@
 import { use, useEffect, useState } from "react";
 
 function App() {
-  const [doRender,setDORender] = useState(false)
-  return (
-    <>
-    {doRender && <Counter />}
-    <button onClick={()=>setDORender(prev => !prev)}>Start a counter</button>
-    </>
-  );
+  const [doRender, setDORender] = useState(false);
+  console.log("counting");
+  useEffect(() => {
+    console.log("mounter");
+    const interval = setInterval(() => {
+      setDORender((prev) => !prev);
+    }, 5000);
+
+    return () => {
+      console.log("unmounted");
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <>{doRender && "Hii There"}</>;
 }
 
 function Counter() {
   const [count, setCount] = useState(0);
 
- useEffect(()=>{
-    setInterval(()=>{
-      setCount(prev=>prev+1)
-    },1000)
- },[])
+  useEffect(() => {
+    const clock = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, []);
   console.log("Counter");
   return (
     <>
       <div>
-        <button>Counter : {count}</button>
+        <div>Counter : {count}</div>
       </div>
     </>
   );
