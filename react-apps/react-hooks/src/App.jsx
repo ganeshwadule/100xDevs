2df12b7898a4883ac2e435cfb5c1b3ab722a98fc
ganeshwadule <uniquegbw09@gmail.com>
2025-04-13 02:23:51 +0530

    tailwind css

--- react-apps/react-hooks/src/App.jsx
+++ react-apps/react-hooks/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react";
-import useFetch from "./hooks/useFetch";
-import UseFetchCompo from "./components/UseFetchCompo";
-import UsePrevCompo from "./components/UsePrevCompo";
-import ContextAPICompo from "./components/ContextAPICompo";
-import RecoilComponent from "./components/RecoilComponent";
-import LanguageContextProvider from "./contexts/LanguageContext";
-import { RecoilRoot } from "recoil";
-import { translations } from "./contexts/LanguageContext";
-import Notifications from "./components/Notifications";
-import Todos from "./components/Todos";
-import Hooks from "./components/Hooks";
 function App() {
   return (
     <>
-      {/* <UseFetchCompo/> */}
-      {/* <UsePrevCompo></UsePrevCompo> */}
-
-      {/* <ContextAPICompo></ContextAPICompo> */}
-      {/* <LanguageContextProvider>
-      <ChangeLanguage/>
-      </LanguageContextProvider> */}
-      {/* <RecoilComponent />
-      <RecoilRoot>
-        <Todos />
-      </RecoilRoot> */}
-
-        <Hooks/>
+      <div className="w-screen h-screen bg-cyan-700 flex p-30 items-center flex-col text-white gap-9">
+        <h1 className="text-3xl">
+          <span className="text-emerald-500">Webinar</span>.gg
+        </h1>
+        <div className="flex flex-col gap-7 justify-center items-center">
+          <div className="font-bold text-xl">Verify Your Age</div>
+          <div className="flex flex-col gap-7 justify-center items-center">
+            <label htmlFor="">
+              Please confirm your birth year.This data will not be stored.
+            </label>
+            <div className="flex flex-col justify-center items-center text-white w-4/6 gap-5">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Your Birth Year"
+                className="border-0 outline-0 bg-gray-500 w-full rounded px-2 py-1"
+              />
+              <button className="font-bold bg-gray-300 text-white bold   border-0 outline-0 rounded  w-full px-2 py-1">
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
