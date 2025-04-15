a76bdd499e5f72ce907626cf8c83ac5e2ff97190
ganeshwadule <uniquegbw09@gmail.com>
2025-04-15 14:21:28 +0530

    Unions & Intersection in Types

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,39 +1,17 @@
-// abstract class User {
-//   name: string;
-//   constructor(name: string) {
-//     this.name = name;
-//   }
-//   abstract greet: () => string;
-// }
-
-// class Manager extends User {
-//   name: string;
-
-//   constructor(name: string) {
-//     super(name);
-//     this.name = name;
-//   }
-
-//   greet = () => {
-//     return "hello";
-//   };
-// }
-
-
-type User = {
+interface User {
   name: string;
   age: number;
 }
 
-type Parent = {
-  name: string;
-  age: number;
-  pancard: number
+const users: User[] = [
+  { name: "Ganesh", age: 1 },
+  { name: "Mahesh", age: 1 },
+  { name: "Ganesh", age: 6 },
+];
+
+function legalUsers(users: User[]): User[] {
+  const legals = users.filter((user) => user.age >= 18);
+  return legals;
 }
 
-// this enforces object to have all props of Parent & User
-let user:User & Parent={
-  name:"ganesh",
-  age:18,
-  pancard:24545
-}
+console.log(legalUsers(users))
